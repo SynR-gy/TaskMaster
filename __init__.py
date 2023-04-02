54c1675c393fcf58e886f533053cea401e14f4dd
SynR_gy <86202211+SynR-gy@users.noreply.github.com>
2023-04-02 23:43:06 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name" : "TaskMaster",
     "author" : "Synrgy",
-    "version" : (1, 7 ,3),
+    "version" : (1, 7 ,4),
     "blender" : (3, 6, 0),
     "location" : "View3d > Tool",
     "warning" : "",
